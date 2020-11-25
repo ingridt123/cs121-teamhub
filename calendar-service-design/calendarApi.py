9465655fc26175421975e12d8bfe1f4fe8d64d49
ingridt123 <ingridszenga@gmail.com>
2020-11-25 11:47:01 -0600

    Create separate copy for calendar-service-design

---
+++ calendar-service-design/calendarApi.py
@@ -0,0 +1,122 @@
+import logging as logger
+from flask import Flask
+
+import calendarEvent
+import calendarRequest
+import calendarApiFirebase
+
+"""
+Team: TeamHub
+Members: Keizo, Pavle, Ingrid
+
+Project: P3C.1 - Component Design
+Author: Ingrid
+
+The calendar service will be responsible for providing all services related to 
+the calendar for the frontend (i.e. its operations will be called by the frontend 
+through its external interface to read and write data to and from database). 
+Specifically, the frontend can call the service to get, create, update and delete 
+event(s) from/to the database. When performing these operations, the calendar 
+service will check with the users service to ensure that the user has access.
+
+Component Specification: https://docs.google.com/document/d/1j7ceIlhty_7w0kBpc7_oYYS5CyyPhoTLfsG4tJXh5QI
+    
+"""
+app = Flask(__name__)
+
+"""
+Rationale: Only the public endpoints are in this file to promote better modularity
+and readability. This file contains all methods in the public interface of the API.
+"""
+
+@app.route('/events', methods=['GET'])
+def getEvents():
+    """
+    Returns a list of the team and individual events for the given user.
+    """
+    # Get JSON from request
+    # If BadRequest exception raised or empty JSON, return ("Error", 400)
+
+    # Create CalendarRequest object to get all setup info for request
+    # Return error if error is thrown, otherwise continue
+
+    # Call calendarApiFirebase.getEvents() to get events from database
+    # Return error if error is returned, otherwise continue
+
+    # Return list of events (in JSON), 200 for success
+    pass
+
+
+@app.route('/events', methods=['POST'])
+def addEvent():
+    """
+    Adds an event into the database with the given information.
+    """
+    # Get JSON from request
+    # If BadRequest exception raised or empty JSON, return ("Error", 400)
+
+    # Create CalendarRequest object to get all setup info for request
+    # Return error if error is thrown, otherwise continue
+
+    # Extract other data for event from JSON in request, create CalendarEvent object (addEvent = True)
+    # Return error if error is returned, otherwise continue
+
+    # Call calendarApiFirebase.addEvent() to add event to database
+    # Return error if error is returned, otherwise continue
+
+    # Return "Success", 201 for success
+    pass
+
+
+@app.route('/events', methods=['PUT'])
+def updateEvent():
+    """
+    Updates the event in the database with the given information.
+    """
+    # Get JSON from request
+    # If BadRequest exception raised or empty JSON, return ("Error", 400)
+
+    # Create CalendarRequest object to get all setup info for request
+    # Return error if error is thrown, otherwise continue
+
+    # Extract other data for event from JSON in request, create CalendarEvent object (addEvent = False)
+    # Return error if error is returned, otherwise continue
+
+    # Call calendarApiFirebase.updateEvent() to update event in database
+    # Return error if error is returned, otherwise continue
+
+    # Return "Success", 201 for success
+    pass
+
+
+@app.route('/events', methods=['DELETE'])
+def deleteEvent():
+    """
+    Deletes the event from the database.
+    """
+    # Get JSON from request
+    # If BadRequest exception raised or empty JSON, return ("Error", 400)
+
+    # Create CalendarRequest object to get all setup info for request
+    # Return error if error is thrown, otherwise continue
+
+    # Extract event id from JSON in request
+    # If no event id provided or invalid, return "Error", 400
+
+    # Call calendarApiFirebase.deleteEvent() to update event in database
+    # Return error if error is returned, otherwise continue
+
+    # Return "Success", 201 for success
+    pass
+
+"""
+Rationale: One alternative suggested during the reviews was using the Connexion
+module for handling the requests and responses of the Flask API. However, given
+that the data from the database will also need to be validated and then processed
+in the calendar service using CalendarEvent objects, this more manual way of 
+handling does not add much additional code and the CalendarEvent class would 
+still need to be used for the validation described earlier, thus it was decided
+that the Connexion module will not be used. If this changes in the future, the
+modular nature of the backend services will allow the module to be used instead
+without affecting other components of the application.
+"""
---
+++ calendar-service-design/calendarApiFirebase.py
@@ -0,0 +1,160 @@
+from google.cloud import firestore
+from firebase_admin import credentials, firestore, initialize_app
+import logging as logger
+
+import calendarEvent
+
+"""
+Helper functions for all Firebase-related operations.
+
+Rationale: All of the operations related to communicating with Firebase is
+put in this class to promote modularity and readability. Only this class will
+need to be altered if we use firebase for another database, for example.
+"""
+
+"""
+Rationale for change: The getFirebaseClient() function was added in response to
+an issue raised during the reviews, since before communicating with the Firebase
+database, a client must be created, with configurations such as the credentials 
+and project ID, to communicate with the Firestore API. In addition, due to the
+changes described above, the functions getEvents(), addEvent(), updateEvent() and
+deleteEvent() have an added firebaseToken parameter to initilize the Firestore
+client. The getEventsReference() will then take in the Firestore client to get
+the reference using this client.
+"""
+def getFirebaseClient(firebaseToken):
+    """
+    Initializes Cloud Firebase database client for interacting with the Google Cloud Firestore API.
+
+    Parameters
+        firebaseToken : str
+            The Firebase token for the given user.
+
+    Returns
+        db : Client
+            A client for interacting with the Firestore API.
+    """
+    # Get Firebase credentials using JSON file and firebaseToken
+    # Initialize Firebase app using credentials
+    # Return Cloud Firestore database client
+    pass
+
+
+def getEventsReference(db, schoolId, teamId, eventId=None):
+    """
+    Gets Firebase database reference for the events of the given school and team id.
+
+    Parameters
+        db : Client
+            A client for interacting with the Firestore API.
+        schoolId : str
+            The user's school id.
+        teamId : str
+            The user's team id.
+        eventId : str, optional
+            The event's id.
+
+    Returns
+        ref : CollectionReference or DocumentReference
+            The reference for the events of the given school and team id.
+    """
+    # Get and return reference:
+    #   schools -> schoolId -> teams -> teamId -> events -> eventId
+    # If eventId is None, then return reference up to events collection.
+    pass
+
+
+def getEvents(firebaseToken, schoolId, teamId, userId):
+    """
+    Gets all events for the given user id with their school and team id.
+
+    Parameters
+        firebaseToken : str
+            The Firebase token for the given user.
+        schoolId : str
+            The user's school id.
+        teamId : str
+            The user's team id.
+        userId : str
+            The user's user id.
+
+    Returns
+        results : {str: <value>}
+            The events returned from the database.
+    """
+    # Calls getFirebaseClient to get a client for interacting with the Firestore API
+    # Calls getEventsReference to get the database reference
+    # Use Firebase function .where().stream() to get events from the database, using the
+    #   conditions where the event has an empty list of userIds or userId is in the list
+    # If fails while getting events, return "Error", 404
+
+    # Jsonifiy and return the results
+    pass
+
+
+def addEvent(firebaseToken, schoolId, teamId, eventDict):
+    """
+    Adds eventDict to the events collection for the given school and team id.
+
+    Parameters
+        firebaseToken : str
+            The Firebase token for the given user.
+        schoolId : str
+            The user's school id.
+        teamId : str
+            The user's team id.
+        eventDict : dict()
+            A dictionary representing the calendar event to be added to the database.
+    """
+    # Calls getFirebaseClient to get a client for interacting with the Firestore API
+    # Calls getEventsReference to get the database reference
+    # Use Firebase function .add() to add eventDict to the database
+    # If user not authorized to add event to database, return "Error", 401
+    # If otherwise fails while adding event, return "Error", 404
+    pass
+
+
+def updateEvent(firebaseToken, schoolId, teamId, eventId, eventDict):
+    """
+    Updates event to eventDict in the events collection for the given school and team id.
+
+    Parameters
+        firebaseToken : str
+            The Firebase token for the given user.
+        schoolId : str
+            The user's school id.
+        teamId : str
+            The user's team id.
+        eventId : str
+            The event's id.
+        eventDict : dict()
+            A dictionary representing the calendar event to be updated in the database.
+    """
+    # Calls getFirebaseClient to get a client for interacting with the Firestore API
+    # Calls getEventsReference to get the database reference
+    # Use Firebase function .update() to update event with eventId to eventDict in the database
+    # If user not authorized to update event in database, return "Error", 401
+    # If otherwise fails while updating event, return "Error", 404
+    pass
+
+
+def deleteEvent(firebaseToken, schoolId, teamId, eventId):
+    """
+    Deletes event with eventId from events collection for the given school and team id.
+
+    Parameters
+        firebaseToken : str
+            The Firebase token for the given user.
+        schoolId : str
+            The user's school id.
+        teamId : str
+            The user's team id.
+        eventId : str
+            The event's id.
+    """
+    # Calls getFirebaseClient to get a client for interacting with the Firestore API
+    # Calls getEventsReference to get the database reference
+    # Use Firebase function .delete() to delete event with eventId from the database
+    # If user not authorized to delete event from database, return "Error", 401
+    # If otherwise fails while deleting event, return "Error", 404
+    pass
