a5fe9baf335a2625d76381596b2e42b01b78f2a1
ingridt123 <ingridszenga@gmail.com>
2020-11-26 12:12:59 -0600

    Commit CalendarRequest before testing

--- calendar-service/calendarRequest.py
+++ calendar-service/calendarRequest.py
@@ -1,12 +1,15 @@
 import logging as logger
+import requests
+
+import calendarErrors
 
 class CalendarRequest:
     """
     A class used to store all information related to the calendar API request.
 
     Attributes
-        json : str
-            A string representing data from the JSON object.
+        json : {str: <value>}
+            A dictionary representing data from the JSON object.
         userToken : str
             The user token passed from the front end.
         firebaseToken : str
@@ -22,12 +25,16 @@ class CalendarRequest:
         Constructor for creating CalendarRequest objects.
 
         Parameters
-            json : str
-                A string representing data from the JSON object.
+            json : {str: <value>}
+                A dictionary representing data from the JSON object.
         """
         # Set the json attribute to the json passed in
+        self.json = json
+        
         # Call the setter methods to populate all attributes
-        pass
+        self.setUserToken()
+        self.setFirebaseToken()
+        self.setSchoolAndTeamId()
 
 
     def getJson(self):
@@ -35,20 +42,24 @@ class CalendarRequest:
         Gets json attribute.
 
         Returns
-            json : str
-                A string representing data from the JSON object.
+            json : {str: <value>}
+                A dictionary representing data from the JSON object.
         """
-        pass
+        return self.json
 
 
     def setUserToken(self):
         """
         Extract user token from request and populate userToken attribute.
         """
-        # Extract user token from JSON in request 
         # If no user token provided or invalid type, throw ("Error", 400)
-        # Otherwise set the userToken attribute
-        pass
+        # Otherwise extract user token from JSON in request and set the userToken attribute
+        if "userToken" not in self.json:
+            raise calendarErrors.Error400("User token was not provided")
+        elif type(self.json["userToken"]) == str:
+            raise calendarErrors.Error400("User token is invalid type: " + str(type(self.json["userToken"])))
+        else:
+            self.userToken = str(self.json["userToken"])
 
 
     def getUserToken(self):
@@ -59,19 +70,25 @@ class CalendarRequest:
             userToken : str
                 The user token passed from the front end.
         """
-        pass
+        return self.userToken
 
 
     def setFirebaseToken(self):
         """
         Gets Firebase token from user token and populates the firebaseToken attribute.
-
-        
         """
         # Call GET /check_token in users service to verify and get Firebase token
         # If invalid token or Firebase token empty, throw ("Error", 401)
+        # try:
+        firebaseToken = requests.get("/check_token", params={'userToken': self.userToken})
+        except Exception:  # TODO: change
+            raise calendarErrors.Error401("User token is invalid")
+
+        if firebaseToken == None or firebaseToken == "":
+            raise calendarErrors.Error401("Firebase token is empty")
+
         # Otherwise set the firebaseToken attribute
-        pass
+        self.firebaseToken = firebaseToken
 
     
     def getFirebaseToken(self):
@@ -82,8 +99,7 @@ class CalendarRequest:
             firebaseToken : str
                 The Firebase token returned from the users service.
         """
-        pass
-
+        return self.firebaseToken
 
 
     def setSchoolAndTeamId(self):
@@ -92,8 +108,19 @@ class CalendarRequest:
         """
         # Call GET /users/current in users service to get user's school and team ids
         # If error or one/both id(s) empty, throw ("Error", 404)
+        try:
+            schoolId, teamId = requests.get("/users/current", params={'userToken': self.userToken})
+        except Exception:  # TODO: change
+            raise calendarErrors.Error404("Error getting school and/or team id")
+
+        if schoolId == None or schoolId == "":
+            raise calendarErrors.Error404("School id is empty")
+        if teamId == None or teamId == "":
+            raise calendarErrors.Error404("Team id is empty")
+
         # Otherwise set the schoolId and teamId attributes
-        pass
+        self.schoolId = schoolId
+        self.teamId = teamId
 
 
     def getSchoolId(self):
@@ -104,7 +131,7 @@ class CalendarRequest:
             schoolId : str
                 The user's school id.
         """
-        pass
+        return self.schoolId
 
 
     def getTeamId(self):
@@ -115,4 +142,4 @@ class CalendarRequest:
             teamId : id
                 The user's team id.
         """
-        pass
+        return self.teamId
