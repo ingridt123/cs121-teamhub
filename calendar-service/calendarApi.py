9bf4239fcd9c86a10c2ae4f14089cb8f4545321a
ingridt123 <ingridszenga@gmail.com>
2020-11-28 20:55:37 -0600

    Commit CalendarApi before testing

--- calendar-service/calendarApi.py
+++ calendar-service/calendarApi.py
@@ -1,9 +1,10 @@
-import logging as logger
-from flask import Flask
+from flask import Flask, jsonify, make_response, request
+from werkzeug.exceptions import BadRequest
 
 import calendarEvent
 import calendarRequest
 import calendarApiFirebase
+import calendarErrors
 
 """
 The calendar service will be responsible for providing all services related to 
@@ -22,15 +23,32 @@ def getEvents():
     """
     # Get JSON from request
     # If BadRequest exception raised or empty JSON, return ("Error", 400)
+    try:
+        json = request.get_json()
+    except request.BadRequest:
+        return "Error", 400
 
     # Create CalendarRequest object to get all setup info for request
     # Return error if error is thrown, otherwise continue
+    try:
+        calRequest = calendarRequest.CalendarRequest(json)
+    except calendarErrors.Error400 as e:
+        return e.message, 400
+    except calendarErrors.Error401 as e:
+        return e.message, 401
 
     # Call calendarApiFirebase.getEvents() to get events from database
     # Return error if error is returned, otherwise continue
+    try:
+        calEvents = calendarApiFirebase.getEvents(calRequest.getFirebaseToken(), \
+            calRequest.getSchoolId(), calRequest.getTeamId(), calRequest.getUserToken())
+    except calendarErrors.Error401 as e:
+        return e.message, 401
+    except calendarErrors.Error404 as e:
+        return e.message, 404
 
-    # Return list of events (in JSON), 200 for success
-    pass
+    # # Return list of events (in JSON), 200 for success
+    return calEvents, 200
 
 
 @app.route('/events', methods=['POST'])
@@ -40,18 +58,39 @@ def addEvent():
     """
     # Get JSON from request
     # If BadRequest exception raised or empty JSON, return ("Error", 400)
+    try:
+        json = request.get_json()
+    except request.BadRequest:
+        return "Error", 400
 
     # Create CalendarRequest object to get all setup info for request
     # Return error if error is thrown, otherwise continue
+    try:
+        calRequest = calendarRequest.CalendarRequest(json)
+    except calendarErrors.Error400 as e:
+        return e.message, 400
+    except calendarErrors.Error401 as e:
+        return e.message, 401
 
     # Extract other data for event from JSON in request, create CalendarEvent object (addEvent = True)
     # Return error if error is returned, otherwise continue
+    try:
+        calEvent = calendarEvent.CalendarEvent(json, True)
+    except calendarErrors.Error400 as e:
+        return e.message, 400
 
     # Call calendarApiFirebase.addEvent() to add event to database
     # Return error if error is returned, otherwise continue
+    try:
+        calendarApiFirebase.addEvent(calRequest.getFirebaseToken(), calRequest.getSchoolId(), \
+            calRequest.getTeamId(), calEvent.toDict())
+    except calendarErrors.Error401 as e:
+        return e.message, 401
+    except calendarErrors.Error404 as e:
+        return e.message, 404
 
     # Return "Success", 201 for success
-    pass
+    return "Success", 201
 
 
 @app.route('/events', methods=['PUT'])
@@ -61,18 +100,39 @@ def updateEvent():
     """
     # Get JSON from request
     # If BadRequest exception raised or empty JSON, return ("Error", 400)
+    try:
+        json = request.get_json()
+    except request.BadRequest:
+        return "Error", 400
 
     # Create CalendarRequest object to get all setup info for request
     # Return error if error is thrown, otherwise continue
+    try:
+        calRequest = calendarRequest.CalendarRequest(json)
+    except calendarErrors.Error400 as e:
+        return e.message, 400
+    except calendarErrors.Error401 as e:
+        return e.message, 401
 
     # Extract other data for event from JSON in request, create CalendarEvent object (addEvent = False)
     # Return error if error is returned, otherwise continue
+    try:
+        calEvent = calendarEvent.CalendarEvent(json, False)
+    except calendarErrors.Error400 as e:
+        return e.message, 400
 
     # Call calendarApiFirebase.updateEvent() to update event in database
     # Return error if error is returned, otherwise continue
+    try:
+        calendarApiFirebase.updateEvent(calRequest.getFirebaseToken(), calRequest.getSchoolId(), \
+            calRequest.getTeamId(), calEvent.getEventId(), calEvent.toDict())
+    except calendarErrors.Error401 as e:
+        return e.message, 401
+    except calendarErrors.Error404 as e:
+        return e.message, 404
 
     # Return "Success", 201 for success
-    pass
+    return "Success", 201
 
 
 @app.route('/events', methods=['DELETE'])
@@ -82,15 +142,31 @@ def deleteEvent():
     """
     # Get JSON from request
     # If BadRequest exception raised or empty JSON, return ("Error", 400)
+    try:
+        json = request.get_json()
+    except request.BadRequest:
+        return "Error", 400
 
     # Create CalendarRequest object to get all setup info for request
     # Return error if error is thrown, otherwise continue
+    try:
+        calRequest = calendarRequest.CalendarRequest(json)
+    except calendarErrors.Error400 as e:
+        return e.message, 400
+    except calendarErrors.Error401 as e:
+        return e.message, 401
 
-    # Extract event id from JSON in request
-    # If no event id provided or invalid, return "Error", 400
+    # Extract other data for event from JSON in request, create CalendarEvent object (addEvent = False)
+    # Return error if error is returned, otherwise continue
+    try:
+        calEvent = calendarEvent.CalendarEvent(json, False)
+    except calendarErrors.Error400 as e:
+        return e.message, 400
 
     # Call calendarApiFirebase.deleteEvent() to update event in database
     # Return error if error is returned, otherwise continue
+    calendarApiFirebase.deleteEvent(calRequest.getFirebaseToken(), calRequest.getSchoolId(), \
+        calRequest.getTeamId(), calEvent.getEventId())
 
     # Return "Success", 201 for success
-    pass
+    return "Success", 201
