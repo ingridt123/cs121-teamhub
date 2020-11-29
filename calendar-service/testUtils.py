65619536dcdea47da9e31adbee72df909a3aa082
ingridt123 <ingridszenga@gmail.com>
2020-11-28 21:50:18 -0600

    Update test utils to reset Firestore emulator before each test case

--- calendar-service/testUtils.py
+++ calendar-service/testUtils.py
@@ -1,9 +1,12 @@
 import pytest
 
 import os
+import google.api_core.datetime_helpers
 from google.cloud import firestore
 import google.auth.credentials
 from unittest import mock
+import time
+import requests
 
 """ Utility functions for testing. """
 
@@ -21,17 +24,52 @@ def db(mocker):
     os.environ["FIRESTORE_HOST"] = "http://localhost:8080"
     os.environ["FIRESTORE_PROJECT_ID"] = "test"
 
-    # Start and import data into emulator
-    # Note: must run in cs121-teamhub directory
-    # os.system("firebase emulators:start --import=./saved-data")
-    # time.sleep(5)
-
+    # Create database client
     credentials = mock.Mock(spec=google.auth.credentials.Credentials)
     db = firestore.Client(project="test", credentials=credentials)
     mocker.patch('calendarApiFirebase.getFirebaseClient', return_value=db)
-    yield db
 
-    # TODO teardown code
-    # os.system("kill $!")
-    # os.system('\x03')
-    # os.system('\x03')
+    # Reset data in emulator
+    requests.delete("http://localhost:8080/emulator/v1/projects/test/databases/(default)/documents")
+    ref = db.collection(u'schools').document(u'school1').collection(u'teams').document('team123').collection(u'events')
+    ref.document(u'0d3IKB9awt0FtzN7z1Qv').set({
+        'userIds': ['user456', 'user234'], 
+        'name': 'Basketball Practice', 
+        'dates': {
+            'from': google.api_core.datetime_helpers.from_rfc3339("2020-11-12T00:00:00.000000Z"),
+            'to': google.api_core.datetime_helpers.from_rfc3339("2020-11-12T00:00:00.000000Z")
+        }, 
+        'eventType': 'practice', 
+        'location': 'Rains'
+    })
+    ref.document(u'6w3mDhxwunaqVOVvUqDG').set({
+        'userIds': [],
+        'name': 'Track Meet',
+        'dates': {
+            'from': google.api_core.datetime_helpers.from_rfc3339("2020-11-08T00:00:00.000000Z"),
+            'to': google.api_core.datetime_helpers.from_rfc3339("2020-11-08T00:00:00.000000Z")
+        },
+        'eventType': 'competition'
+    })
+    ref.document(u'gkjtuYvCOGkiVeEsmD7T').set({
+        'userIds': ['user123', 'user321'], 
+        'name': 'Soccer Practice',
+        'location': 'Parents', 
+        'eventType': 'practice', 
+        'times': {
+            'from': google.api_core.datetime_helpers.from_rfc3339("2020-12-10T07:45:00.000000Z"),
+            'to': google.api_core.datetime_helpers.from_rfc3339("2020-12-10T08:00:00.000000Z")
+        },
+        'dates': {
+            'to': google.api_core.datetime_helpers.from_rfc3339("2020-12-10T00:00:00.000000Z"),
+            'from': google.api_core.datetime_helpers.from_rfc3339("2020-12-10T00:00:00.000000Z"),
+        },
+        'repeating': {
+            'frequency': 'w', 
+            'daysOfWeek': ['M', 'W'], 
+            'startDate': google.api_core.datetime_helpers.from_rfc3339("2020-12-10T00:00:00.000000Z"),
+            'endDate': google.api_core.datetime_helpers.from_rfc3339("2021-01-10T00:00:00.000000Z"),
+        }, 
+    })
+
+    yield db
